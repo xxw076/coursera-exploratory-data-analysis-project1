ccfcadec7ea850e02d23e00a616f11349cf682eb
xxw076 <113251998+xxw076@users.noreply.github.com>
2022-09-18 22:27:50 +0100

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,16 @@
+#Setting working directory
+setwd("~/R/Coursera/exdata%2Fdata%2Fhousehold_power_consumption")
+#Reading, naming and subsetting power consumption data
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+#calling the basic plot function
+hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+
+# annotating graph
+title(main="Global Active Power")
+
+#copying to png
+dev.copy(png, filename="plot1.png")
+dev.off()
